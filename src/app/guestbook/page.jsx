30c06c556261553809414050a94f9dd53f507e2c
Gylan Salih <Gylan.Salih@outlook.de>
2025-01-26 02:50:36 +0100

    Guestbook

--- src/app/guestbook/page.jsx
+++ src/app/guestbook/page.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { useEffect } from "react";
+import './guestbook.css';  // Importiere die externe CSS-Datei
 
 export default function Guestbook() {
   useEffect(() => {
@@ -8,7 +9,7 @@ export default function Guestbook() {
     script.src = "https://giscus.app/client.js";
     script.async = true;
     script.setAttribute("data-repo", "GylanSalih/NextJS-Portify");
-    script.setAttribute("data-default-comment-order", "newest");
+    script.setAttribute("data-default-comment-order", "oldest");
     script.setAttribute("data-repo-id", "R_kgDONsIjJw");
     script.setAttribute("data-category", "General");
     script.setAttribute("data-category-id", "DIC_kwDONsIjJ84CmWBt");
@@ -17,26 +18,17 @@ export default function Guestbook() {
     script.setAttribute("data-reactions-enabled", "1");
     script.setAttribute("data-emit-metadata", "0");
     script.setAttribute("data-input-position", "bottom");
-    script.setAttribute("data-theme", "dark");
+    script.setAttribute("data-theme", "dark");  // Setze das benutzerdefinierte Theme
     script.setAttribute("data-lang", "de");
     script.setAttribute("data-loading", "lazy");
     script.setAttribute("crossorigin", "anonymous");
     document.getElementById("giscus-container").appendChild(script);
   }, []);
 
-
-
   return (
-    <div 
-      className="container mx-auto"
-      style={{
-        padding: "2rem", // Increased padding for larger spaces
-        paddingTop: "120px", // Slightly bigger top padding
-        width: "60%", // Made width a bit wider
-      }}
-    >
-      <h1 className="text-2xl font-bold mb-4">Guestbook</h1>
-      <p className="mb-4">Leave a message in the guestbook!</p>
+    <div className="container mx-auto">
+      <h1 className="guestbook-title">Guestbook</h1>
+      <p className="guestbook-description">Leave a message in the guestbook!</p>
       <div id="giscus-container"></div>
     </div>
   );
