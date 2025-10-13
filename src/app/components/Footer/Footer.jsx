ef733d4acd05950a5ce0dd7ae39549418425d01e
Japhet Adofo-Adjei <jphtadjei@gmail.com>
2025-10-13 20:45:53 +0000

    edited footer

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Github, Linkedin, Dribbble, Mail } from 'lucide-react';
+import { Github, Linkedin, Dribbble, Mail, Instagram } from 'lucide-react';
 import './Footer.css';
 
 const Footer = () => {
@@ -8,42 +8,32 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-bottom">
           <div className="footer-left">
-            <span className="footer-copyright">© Gylan Salih</span>
-            <span className="footer-legal">
-              <a
-                href="https://github.com/GylanSalih/NextJS-Portify/tree/main"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                If you like the website, feel free to visit the open-source
-                repository!
-              </a>
-            </span>
+            <span className="footer-copyright">© Japhet Adofo-Adjei</span>
           </div>
 
           <div className="footer-socials">
             <a
-              href="https://github.com/GylanSalih/"
+              href="https://github.com/Jay-Adjei"
               target="_blank"
               rel="noopener noreferrer"
             >
               <Github className="footer-icon" />
             </a>
             <a
-              href="https://linkedin.com"
+              href="https://www.linkedin.com/in/japhet-adofo-adjei-347706256/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <Linkedin className="footer-icon" />
             </a>
             <a
-              href="https://dribbble.com"
+              href="https://www.instagram.com/_.mr.adjei/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Dribbble className="footer-icon" />
+              <Instagram className="footer-icon" />
             </a>
-            <a href="mailto:hello@portfolio.com">
+            <a href="mailto:mr.adjei17@gmail.com">
               <Mail className="footer-icon" />
             </a>
           </div>
